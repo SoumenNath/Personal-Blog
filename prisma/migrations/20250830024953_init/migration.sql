9a24f63ce13441d00af15a1010c9b2d5bd8992bb
SoumenNath <soumennath120@gmail.com>
2025-08-29 22:51:47 -0400

    initial commit

---
+++ prisma/migrations/20250830024953_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
